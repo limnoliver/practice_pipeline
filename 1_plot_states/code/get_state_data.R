84777c8b14c383d6043e1e6ad903ec7d83762be4
Kelsey J Vitense <viten003@ad.umn.edu>
2019-06-24 12:38:35 -0500

    Build iowa dataset and plots. Write out minnesota and iowa CSVs. Add sample size option to get_state_data.R

--- 1_plot_states/code/get_state_data.R
+++ 1_plot_states/code/get_state_data.R
@@ -1,4 +1,4 @@
-get_state_data <- function(my_state) {
+get_state_data <- function(my_state, samp_size) {
   state_dat <- 
     maps::map(database = 'state', region = my_state, plot = FALSE)
   
@@ -6,9 +6,9 @@ get_state_data <- function(my_state) {
   long_seq <- seq(coord_range[1], coord_range[2], by = 0.01)
   lat_seq <- seq(coord_range[3], coord_range[4], by = 0.01) 
   
-  state_dat <- data.frame(value = sample(1:100, 20, replace = TRUE),
-                          long = sample(long_seq, 20),
-                          lat = sample(lat_seq, 20),
+  state_dat <- data.frame(value = sample(1:100, samp_size, replace = TRUE),
+                          long = sample(long_seq, samp_size),
+                          lat = sample(lat_seq, samp_size),
                           state = my_state)
   
   return(state_dat)
